ba6cdec819ec81735a15762ffd0de4bde23d5a4f
TalaatMagdy <talaatmagdy75@gmail.com>
2020-01-03 23:27:25 +0200

    added Test for models

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Publisher, type: :model do
   it { should validate_presence_of(:name) }
 
+  # An author should indeed have many books!
+  it { should have_many(:books) }
+
   it 'has a valid factory' do
     expect(build(:publisher)).to be_valid
   end
