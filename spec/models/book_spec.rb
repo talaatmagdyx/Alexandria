5eb49aac7d59b7e40831b7574b60daea420e3bc8
TalaatMagdy <talaatmagdy75@gmail.com>
2020-01-06 03:02:49 +0200

    Add BooksController, BookPresenter and FieldPicker

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Book, type: :model do
   it { should validate_uniqueness_of(:isbn_10) }
   it { should validate_uniqueness_of(:isbn_13) }
 
-  it { should belong_to(:publisher).required(false ) }
+  it { should belong_to(:publisher).required(false) }
   it { should belong_to(:author) }
 
   it 'has a valid factory' do
